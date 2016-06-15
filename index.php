a986b9587affac2c69e64e9a49dfce689c89b512
Dávid Naményi <david@joszaki.hu>
2016-06-16 01:17:55 +0200

    Added sample Yo task and made the queue work

--- index.php
+++ index.php
@@ -5,3 +5,12 @@
  */
 
 require 'vendor/autoload.php';
+
+Resque::setBackend('localhost:6379');
+
+$args = [
+    'text' => 'Yooo, it works'
+];
+Resque::enqueue('yo_queue', 'Jobs\Yo', $args);
+
+echo '<hr> end';
---
+++ jobs/Yo.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author David Namenyi <dnamenyi@gmail.com>
+ * Date: 2016.06.15.
+ */
+
+namespace Jobs;
+
+class Yo {
+
+    public function perform()
+    {
+        file_put_contents('output/yo.txt', $this->args['text']);
+    }
+
+}
